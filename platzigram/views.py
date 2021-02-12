77380d18a04d674baaa664584f09f91a876fd97f
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-02-11 22:42:52 -0300

    Improving time

--- platzigram/views.py
+++ platzigram/views.py
@@ -5,5 +5,5 @@ def hello_world(request):
   return HttpResponse("Hello, world!")
 
 def time(request):
-  now = datetime.now()
+  now = datetime.now().strftime('%b %dth, %Y - %H:%M hs')
   return HttpResponse("Que onda!. La hora es: {}".format(str(now)))
