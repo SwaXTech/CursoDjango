51e714ef6b3fb3f1e32d049c910d450d2f82daec
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-02-14 00:04:14 -0300

    Adding posts to template with python logic

--- posts/views.py
+++ posts/views.py
@@ -6,30 +6,30 @@ posts = [
     'title': 'Mont Blac',
     'user': {
       'name': "Yésica Cortéz",
-      'picture': 'https://picsum.photos/200/200/?image=1027'
+      'picture': 'https://picsum.photos/60/60/?image=1027'
     },
     'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hs'),
-    'picture':  'https://picsum.photos/200/200/?image=1036'
+    'picture':  'https://picsum.photos/800/600/?image=1036'
   },
 
   {
     'title': 'Vía Lactea',
     'user': {
       'name': "Christian Van der Henst",
-      'picture': 'https://picsum.photos/200/200/?image=1005'
+      'picture': 'https://picsum.photos/60/60/?image=1005'
     },
     'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hs'),
-    'picture': 'https://picsum.photos/200/200/?image=903'
+    'picture': 'https://picsum.photos/800/600/?image=903'
   },
 
   {
     'title': 'Nuevo Auditorio',
     'user': {
       'name': "Uriel (thespianartist)",
-      'picture': 'https://picsum.photos/200/200/?image=883'
+      'picture': 'https://picsum.photos/60/60/?image=883'
     },
     'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hs'),
-    'picture': 'https://picsum.photos/200/200/?image=1076'
+    'picture': 'https://picsum.photos/800/600/?image=1076'
   }
 ]
 
