ff9140de072ce655c8f31d7cdb490c547d09a61d
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-02-17 21:25:07 -0300

    Added class for user on DB and made migrations.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.db import models # Nos permite interactuar con el ORM
 
-# Create your models here.
+# Definimos una tabla a través de la clase
+
+class User(models.Model):
+
+  email       = models.EmailField(unique = True)
+  password    = models.CharField(max_length = 100)
+  first_name  = models.CharField(max_length = 100)
+  last_name   = models.CharField(max_length = 100)
+  bio         = models.TextField(blank = True)
+  birthdate   = models.DateField(blank = True, null = True)
+  created     = models.DateTimeField(auto_now_add = True) # Fecha en la que se agregó
+  modified    = models.DateTimeField(auto_now = True)    # Fecha en la que se editó
+
+
+  ## Posterior a la creación de esta clase, es necesario realizar la migración. (python manage.py makemigrations)
+
+
+
+
+
