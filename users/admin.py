6a2cc7030203c63c0a2cc9591fc623f210192ad1
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-02-20 21:21:00 -0300

    Now profiles can be filtered

--- users/admin.py
+++ users/admin.py
@@ -7,3 +7,4 @@ class ProfileAdmin(admin.ModelAdmin):
   list_display_links = ('pk', 'user')
   list_editable = ('phone_number', 'website', 'picture')
   search_fields = ('user__username', 'user__email', 'user__first_name', 'user__last_name', 'phone_number')
+  list_filter = ('created', 'modified', 'user__is_active', 'user__is_staff')
