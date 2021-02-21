2f275a4c947222820baf6a20e3a462e9448b53d7
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-02-20 21:39:19 -0300

    Now can create profile while creating user

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from users.models import Profile
 
 @admin.register(Profile)
@@ -24,4 +26,17 @@ class ProfileAdmin(admin.ModelAdmin):
 
   readonly_fields = ('created', 'modified')
 
+
+class ProfileInline(admin.StackedInline):
+
+  model = Profile
+  can_delete = False
+  verbose_name_plural = "profiles"
+
+
+class UserAdmin(BaseUserAdmin):
+  inlines = (ProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
   
