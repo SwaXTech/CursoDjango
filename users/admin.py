fde8496875d8922a873e570578758797e2b51a9a
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-02-20 21:42:09 -0300

    Now showing more details on user

--- users/admin.py
+++ users/admin.py
@@ -36,6 +36,7 @@ class ProfileInline(admin.StackedInline):
 
 class UserAdmin(BaseUserAdmin):
   inlines = (ProfileInline,)
+  list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff')
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
