7830066f28539a6b34682dde7141b8abe99fe635
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-02-20 21:11:58 -0300

    Now phone number redirects to user details

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,4 @@ from users.models import Profile
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
   list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
-
+  list_display_links = ('user', 'phone_number')
