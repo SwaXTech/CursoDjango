b5434e1a5ebadc02c538d6ae96377ddec3b92dc8
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-02-20 21:20:11 -0300

    Now profiles are searchable

--- users/admin.py
+++ users/admin.py
@@ -6,3 +6,4 @@ class ProfileAdmin(admin.ModelAdmin):
   list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
   list_display_links = ('pk', 'user')
   list_editable = ('phone_number', 'website', 'picture')
+  search_fields = ('user__username', 'user__email', 'user__first_name', 'user__last_name', 'phone_number')
