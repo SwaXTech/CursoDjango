30ac21f3eefb1d4f27b38b025e1f50793f616331
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-02-20 21:54:48 -0300

    Now can display pictures

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from django.contrib import admin
 from platzigram import views as local_views
 from posts import views as posts_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('hello-world', local_views.hello_world),
@@ -27,6 +29,6 @@ urlpatterns = [
     path('hello/<str:name>/<int:age>/', local_views.hello),
     path('posts/', posts_views.list_posts),
     path('admin/', admin.site.urls)
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
